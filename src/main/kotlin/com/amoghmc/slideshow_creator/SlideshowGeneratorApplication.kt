cbb10e108c617619b50f47ca1de7cb70b2c7c13b
amoghmc <amoghmc@github.com>
2023-03-04 01:27:43 +0530

    initial ci

---
+++ src/main/kotlin/com/amoghmc/slideshow_creator/SlideshowGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.amoghmc.slideshow_creator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SlideshowGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SlideshowGeneratorApplication>(*args)
+}
