2fae761a2d94c25bb592ab2392297b0b1c6e2989
amoghmc <amoghmc@github.com>
2023-03-04 22:46:31 +0530

    rename main func

--- src/main/kotlin/com/amoghmc/slideshow_creator/SlideshowCreatorApplication.kt
+++ src/main/kotlin/com/amoghmc/slideshow_creator/SlideshowCreatorApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SlideshowGeneratorApplication
+class SlideshowCreatorApplication
 
 fun main(args: Array<String>) {
-	runApplication<SlideshowGeneratorApplication>(*args)
+	runApplication<SlideshowCreatorApplication>(*args)
 }
