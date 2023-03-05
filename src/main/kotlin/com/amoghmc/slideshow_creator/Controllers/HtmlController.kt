e4cf219209ef62a62fa40814505c81e157a263f4
amoghmc <amoghmc@github.com>
2023-03-05 20:53:03 +0530

    add greeting

--- src/main/kotlin/com/amoghmc/slideshow_creator/Controllers/HtmlController.kt
+++ src/main/kotlin/com/amoghmc/slideshow_creator/Controllers/HtmlController.kt
@@ -1,9 +1,13 @@
 package com.amoghmc.slideshow_creator.Controllers
 
+import com.amoghmc.slideshow_creator.Greeting
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+
 
 @Controller
 class HtmlController {
@@ -12,6 +16,12 @@ class HtmlController {
 	fun home(model: Model): String {
 		model["title"] = "Homepage"
 		return "home"
+	}
+
+	@PostMapping("/")
+	fun greetingSubmit(@ModelAttribute greeting: Greeting?, model: Model): String? {
+		model.addAttribute("greeting", greeting)
+		return "result"
 	}
 
 }
---
+++ src/main/kotlin/com/amoghmc/slideshow_creator/Greeting.kt
@@ -0,0 +1,6 @@
+package com.amoghmc.slideshow_creator
+
+class Greeting {
+	var id: Long = 0
+	var content: String? = null
+}
